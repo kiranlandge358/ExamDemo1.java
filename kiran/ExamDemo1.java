feed83b9322171006c3a1f3d1b8df930d949b653
unknown <you@example.com>
2022-09-13 19:48:37 +0530

    add my list

---
+++ kiran/ExamDemo1.java
@@ -0,0 +1,19 @@
+package com.kiran;
+
+import java.util.LinkedList;
+
+public class ExamDemo1 {
+
+	public static void main(String[] args) {
+			LinkedList al = new LinkedList();
+			al.add("ram");
+			al.add(48);
+			al.add("pune");
+			al.add("50");
+			System.out.println(al);
+
+				}
+
+	}
+
+
